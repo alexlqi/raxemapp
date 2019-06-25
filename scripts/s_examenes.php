cce961e4f222709a4615f53817b19cb348ffc646
alexlqi <alexlqi@users.noreply.github.com>
2019-06-25 17:45:10 -0500

    Update s_examenes.php

--- scripts/s_examenes.php
+++ scripts/s_examenes.php
@@ -8,6 +8,7 @@ $dsnModelo=$dsnExamenes;
 $r["err"]=true;
 $ctrl=@$_POST["ctrl"];
 unset($_POST["ctrl"]);
+@file_put_contents(__DIR__."/../debug/examenes_".time().".txt", json_encode([@$_POST,$_SESSION],JSON_PRETTY_PRINT));
 
 
 //TAG NAMES PARA MOSTRAR Y ACUALIZAR POR KEYS
